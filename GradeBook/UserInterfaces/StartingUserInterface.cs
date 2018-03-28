605b2d5cc88489318a31943b2d4324bf78067241
CapnWhizBang <37597308+CapnWhizBang@users.noreply.github.com>
2018-03-27 22:24:42 -0400

    Added IsWeighted parameter

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -6,6 +6,7 @@ namespace GradeBook.UserInterfaces
     public static class StartingUserInterface
     {
         public static bool Quit = false;
+        public static bool IsWeighted = false;
         public static void CommandLoop()
         {
             while (!Quit)
@@ -33,22 +34,23 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted ( true/false).");
                 return;
             }
             var name = parts[1];
             var type = parts[2].ToLower();
+            bool weighted = bool.Parse(parts[3]);
             BaseGradeBook gradeBook;
             //BaseGradeBook gradeBook = new BaseGradeBook(name);
             if (type == "standard")
             {
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name, weighted);
             }
             else if (type == "ranked")
             {
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name, weighted);
             }
             else
             {
